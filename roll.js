6e59c0ee47091f6c7cd43191110afc098e4e05aa
Xilong Yang <xilong.yang@foxmail.com>
2021-09-22 22:19:42 +0800

    update

--- roll.js
+++ roll.js
@@ -2,7 +2,7 @@ var foods = new Array(
 "友园","桃李园","三四","魔饭堂","老汤拉面"
 ,"咖喱饭","河南烩面","简单味道","烤肉饭","魔蜀冒菜"
 ,"牛肉汤","三杯鸡","味恋客","一碗好饭","萌锅蒸饭"
-,"蒸饺"
+,"蒸饺","今日特价"
 );
 function Roll() {
     result = document.getElementById("result");
