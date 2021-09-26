0a86c18e94561b333d18f8f3ba540462c902738d
Xilong Yang <xilong.yang@foxmail.com>
2021-09-26 17:07:34 +0800

    category

--- roll.js
+++ roll.js
@@ -1,13 +1,34 @@
-var foods = new Array(
+var foods = new Map();
+
+foods.set("全部", [
 "友园","桃李园","三四","魔饭堂","老汤拉面"
-,"咖喱饭","河南烩面","简单味道","烤肉饭","魔蜀冒菜"
+,"咖喱饭","河南烩面","简单味道","烤肉饭"
 ,"牛肉汤","三杯鸡","味恋客","一碗好饭","萌锅蒸饭"
-,"蒸饺","今日特价","淮南汤饭","朱家小馆"
-);
+,"蒸饺","淮南汤饭","朱家小馆", "今日特价"
+]);
+
+foods.set("外卖", [
+"魔饭堂","老汤拉面","咖喱饭","河南烩面","简单味道"
+,"烤肉饭","牛肉汤","三杯鸡","味恋客","一碗好饭"
+,"萌锅蒸饭","蒸饺","淮南汤饭","朱家小馆", "今日特价"
+]);
+
+foods.set("食堂", ["友园","桃李园","三四"]);
+
+foods.set("早餐", ["小笼包"]);
+
+foods.set("校外", ["烤鸭", "水果捞", "烤鸡", "比萨"])
+
 function Roll() {
     result = document.getElementById("result");
-    randNum = Math.floor(Math.random() * foods.length);
-    result.innerHTML = foods[randNum];
+
+    select = document.getElementById("select");
+    selectText = select.options[select.selectedIndex].text;
+
+    selectFoods = foods.get(selectText);
+
+    randNum = Math.floor(Math.random() * selectFoods.length);
+    result.innerHTML = selectFoods[randNum];
 
     button = document.getElementById("button");
     button.innerHTML = "重来";
